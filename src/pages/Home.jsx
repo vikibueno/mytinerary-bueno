d269833bea5d806493b5348968465d1333b84dc5
Vik <mviki.bueno@gmail.com>
2023-08-10 05:35:54 -0300

    effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,21 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import Carousel from "../components/Carousel"
+import axios from "axios"
 
 export default function Home() {
-    const [shot,setShow] = useState(true)
-    const titleMain = "Find the perfect destination"
-    let data = [
-      {id: 'america1', city: "Cancun", photo: "/img/america/cancun.jpg"},
-      {id: 'america2', city: "New York", photo: "/img/america/newyork.jpg"},
-      {id: 'america3', city: "Rio de Janeiro", photo: "/img/america/rioDeJaneiro.jpg"},
-      {id: 'america4', city: "Ushuaia", photo: "/img/america/ushuaia.jpg"},
-      {id: 'asia1' , city: "Bangkok", photo: "/img/asia/bangkok.jpg"},
-      {id: 'asia2' , city: "Pekin", photo: "/img/asia/pekin.jpg"},
-      {id: 'asia3' , city: "Singapur", photo: "/img/asia/singapur.jpg"},
-      {id: 'asia4' , city: "Tokyo", photo: "/img/asia/tokio.jpg"},
-      {id: 'europe1' , city: "Ibiza", photo: "/img/europe/ibiza.jpg"},
-      {id: 'europe2' , city: "London", photo: "/img/europe/london.jpg"},
-      {id: 'europe3' , city: "Paris", photo: "/img/europe/paris.jpg"},
-      {id: 'europe4' , city: "Roma", photo: "/img/europe/roma.jpg"},
-      {id: 'oceania1' , city: "Majuro", photo: "/img/oceania/majuro.jpg"},
-      {id: 'oceania2' , city: "Sidney", photo: "/img/oceania/sidney.jpg"},
-      {id: 'oceania3' , city: "Suva", photo: "/img/oceania/suva.jpg"},
-      {id: 'oceania4' , city: "Wellington", photo: "/img/oceania/wellington.jpg"}
-    ]
 
+    const [show,setShow] = useState(true)
+    const [data,setData] = useState([]) 
+    const titleMain = "Find the perfect destination"
+    useEffect(
+        ()=>{
+            axios('/data.json')
+                .then(res=>setData(res.data))
+                .catch(err=>console.log(err))
+        },
+        []
+    )
+    
     return (
     <main className="lg:w-4/5 lg:h-[270px] lg:mt-[168px] lg:mb-4 flex">
       <div className="lg:w-1/2 lg:h-[250px] flex flex-col">
